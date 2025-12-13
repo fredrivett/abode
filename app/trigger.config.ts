e1b483ea63e69bfefeb38e6fc3720ea8b7272c7d
Fred Rivett <fred@fredrivett.com>
2025-12-12 19:16:05 -0800

    add `prismaExtension` to `trigger.config.ts`

--- app/trigger.config.ts
+++ app/trigger.config.ts
@@ -1,3 +1,4 @@
+import { prismaExtension } from "@trigger.dev/build/extensions/prisma";
 import { defineConfig } from "@trigger.dev/sdk/v3";
 
 export default defineConfig({
@@ -19,4 +20,15 @@ export default defineConfig({
     },
   },
   dirs: ["trigger"],
+  build: {
+    extensions: [
+      prismaExtension({
+        mode: "legacy",
+        version: "6.19.0",
+        schema: "prisma/schema.prisma",
+        migrate: false,
+        directUrlEnvVarName: "DIRECT_URL",
+      }),
+    ],
+  },
 });
