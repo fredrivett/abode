b7525a8199a65a94149539d49b1f841f7f1b4038
Fred Rivett <fred@fredrivett.com>
2025-12-23 16:41:51 -0800

    fix bg of OR separateor in enter code form

--- app/src/app/(auth)/join/enter-code-form.tsx
+++ app/src/app/(auth)/join/enter-code-form.tsx
@@ -61,7 +61,7 @@ export function EnterCodeForm() {
             <span className="w-full border-t border-gray-200 dark:border-gray-800" />
           </div>
           <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-white px-2 text-gray-500 dark:bg-gray-950 dark:text-gray-400">
+            <span className="bg-background px-2 text-gray-500 dark:text-gray-400">
               or
             </span>
           </div>
